bf10eebb52f6188b4fbba7681f1af48f8c95e955
lakatostamas <mail.tamaslakatos@gmail.com>
2019-02-16 14:19:03 +0100

    fix: component path

--- src/components/WebcamView/WebcamView.test.js
+++ src/components/WebcamView/WebcamView.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import WebcamView from '../WebcamView/WebcamView';
+import WebcamView from './WebcamView';
 
 const props = {
   position: {
